223aa5f85a881b9664a2a1445e85bb4c7bbc70d8
RST5150 <rstvh5150@gmail.com>
2024-12-01 21:48:35 -0300

    feat: Post Fixeado

--- src/columna/columna.schema.ts
+++ src/columna/columna.schema.ts
@@ -3,10 +3,10 @@ import { Material } from '../columna/columna.entity.js'
 
 const NroSerieLength = 15
 const Mat = Material
-const [FECHA_MIN, FECHA_MAX] = [new Date(1970,1,1), new Date()]
+const [FECHA_MIN, FECHA_MAX] = [new Date(1970, 1, 1).toISOString(), new Date().toISOString()]
 const ERR_NRO_SERIE = `El número de serie debe tener ${NroSerieLength} caracteres`
 const ERR_MATERIAL = `El material debe ser uno de ${Object.values(Mat).join(', ')}`
-const ERR_FECHA = `La fecha debe estar no nula`
+const ERR_FECHA = `La fecha debe estar entre ${FECHA_MIN} y ${FECHA_MAX}`
 const ERR_ID = `Es necesario un id`
 
 const id = v.pipe(
@@ -25,13 +25,15 @@ const material = v.pipe(
 )
 
 const fechaAdquisicion = v.pipe(
-    v.date(ERR_FECHA),
+    v.string(),
+    v.isoDate(ERR_FECHA),
     v.minValue(FECHA_MIN, ERR_FECHA),
     v.maxValue(FECHA_MAX, ERR_FECHA),
 )
 
 const fechaInstalacion = v.optional(v.pipe(
-    v.date(ERR_FECHA),
+    v.string(),
+    v.isoDate(ERR_FECHA),
     v.minValue(FECHA_MIN, ERR_FECHA),
     v.maxValue(FECHA_MAX, ERR_FECHA),
 ))
@@ -41,6 +43,7 @@ export const columnaSchema = v.object({
     material: material,
     fechaAdquisicion: fechaAdquisicion,
     fechaInstalacion: v.optional(fechaInstalacion),
+    review: v.optional(v.array(id)),
 })
 
 export const validateColumna = (input: unknown) => v.safeParseAsync(columnaSchema, input)
