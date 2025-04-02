509415c37aa887f4851eb1bb9bbc6aa1978542ac
RST5150 <rstvh5150@gmail.com>
2025-04-02 14:19:46 -0300

    CRUD serviciosLuz Funcional

--- backend/src/servicioLuz/servicioLuz.entity.ts
+++ backend/src/servicioLuz/servicioLuz.entity.ts
@@ -1,4 +1,13 @@
-import { Entity, Property, ManyToOne, Collection, Cascade, OneToMany, OneToOne, Rel } from "@mikro-orm/core";
+import {
+  Entity,
+  Property,
+  ManyToOne,
+  Collection,
+  Cascade,
+  OneToMany,
+  OneToOne,
+  Rel,
+} from "@mikro-orm/core";
 import { Base } from "../shared/db/base.entity.js";
 import { Luminaria } from "../luminaria/luminaria.entity.js";
 import { EquipoAux } from "../equipoAuxiliar/equipoAuxiliar.entity.js";
@@ -8,26 +17,33 @@ import { Empleado } from "../empleado/empleado.entity.js";
 
 @Entity()
 export class ServicioLuz extends Base {
-    @Property({nullable: false})
-    fechaInstalacion!: Date;
+  @Property({ nullable: false })
+  fechaInstalacion!: Date;
 
-    @OneToMany(() => Luminaria, luminaria => luminaria.serviciosLuz, {lazy: true})
-    luminarias = new Collection<Luminaria>(this);
+  @OneToMany(() => Luminaria, (luminaria) => luminaria.serviciosLuz, {
+    lazy: true,
+  })
+  luminarias = new Collection<Luminaria>(this);
 
-    @OneToMany(() => EquipoAux, equipoAux => equipoAux.serviciosLuz, {lazy: true})
-    equipoAuxiliar = new Collection<EquipoAux>(this);
+  @OneToMany(() => EquipoAux, (equipoAux) => equipoAux.serviciosLuz, {
+    lazy: true,
+  })
+  equipoAuxiliar = new Collection<EquipoAux>(this);
 
-    @OneToOne(() => Columna, (columna) => columna.servicio, {
-        cascade: [Cascade.ALL],
-        owner: true,
-        nullable: true,
-    })
-    columna!: Rel<Columna>;
+  @OneToOne(() => Columna, (columna) => columna.servicio, {
+    cascade: [Cascade.PERSIST, Cascade.MERGE],
+    owner: true,
+    nullable: true,
+  })
+  columna!: Rel<Columna>;
 
-    @OneToMany(() => Mantenimiento, mantenimiento => mantenimiento.servicioLuz, {lazy: true})
-    mantenimientos = new Collection<Mantenimiento>(this);
+  @OneToMany(
+    () => Mantenimiento,
+    (mantenimiento) => mantenimiento.servicioLuz,
+    { lazy: true }
+  )
+  mantenimientos = new Collection<Mantenimiento>(this);
 
-    // @ManyToOne(() => Ubicacion, { nullable: false })         Luego veremos si usamos esto o implementamos una api de mapas 
-    // ubicacion!: Rel<Ubicacion>;
-
+  // @ManyToOne(() => Ubicacion, { nullable: false })         Luego veremos si usamos esto o implementamos una api de mapas
+  // ubicacion!: Rel<Ubicacion>;
 }
--- backend/src/servicioLuz/servicioLuz.schema.ts
+++ backend/src/servicioLuz/servicioLuz.schema.ts
@@ -1,52 +1,44 @@
-import * as v from 'valibot'
-import { Luminaria } from '../luminaria/luminaria.entity.js'
-import { EquipoAux } from '../equipoAuxiliar/equipoAuxiliar.entity.js'
+import * as v from "valibot";
+import { Luminaria } from "../luminaria/luminaria.entity.js";
+import { EquipoAux } from "../equipoAuxiliar/equipoAuxiliar.entity.js";
 
-const ERR_ID = `Es necesario un id`
-const ERR_ID_LUMINARIA = `Es necesario al menos un id de luminaria`
-const ERR_ID_LUMINARIA_MAX = `No se pueden instalar más de 5 luminarias`
-const ERR_ID_EQUIPO_AUX = `Es necesario al menos un id de equipo auxiliar`
-const ERR_ID_EQUIPO_AUX_MAX = `No se pueden instalar más de 5 equipos auxiliares`
-const ERR_ID_COLUMNAS = `Es necesario un id de columna`
-const ERR_FECHA = `La fecha debe ser una fecha válida`
-const ARRAY_ERROR = `Si ves esto, no estas enviando un array pa`
+const ERR_ID = `Es necesario un id`;
+const ERR_ID_LUMINARIA = `Es necesario al menos un id de luminaria`;
+const ERR_ID_LUMINARIA_MAX = `No se pueden instalar más de 5 luminarias`;
+const ERR_ID_EQUIPO_AUX = `Es necesario al menos un id de equipo auxiliar`;
+const ERR_ID_EQUIPO_AUX_MAX = `No se pueden instalar más de 5 equipos auxiliares`;
+const ERR_ID_COLUMNAS = `Es necesario un id de columna`;
+const ERR_FECHA = `La fecha debe ser una fecha válida`;
+const ARRAY_ERROR = `Si ves esto, no estas enviando un array con los datos adecuados pa`;
 
-const id = v.pipe(
-    v.number(ERR_ID),
-    v.integer(ERR_ID),
-)
+const id = v.pipe(v.number(ERR_ID), v.integer(ERR_ID));
 
-const fechaInstalacion = v.pipe(
-    v.string(),
-    v.isoDate(ERR_FECHA),
-)
+const fechaInstalacion = v.pipe(v.string(), v.isoDate(ERR_FECHA));
 
 const luminaria = v.pipe(
-    v.array(v.number(ARRAY_ERROR)),
-    v.minLength(1, ERR_ID_LUMINARIA),
-    v.maxLength(5, ERR_ID_LUMINARIA_MAX),
-)
+  v.array(v.number(ARRAY_ERROR)),
+  v.minLength(1, ERR_ID_LUMINARIA),
+  v.maxLength(5, ERR_ID_LUMINARIA_MAX)
+);
 
 const equipoAux = v.pipe(
-    v.array(v.number(ARRAY_ERROR)),
-    v.minLength(1, ERR_ID_EQUIPO_AUX),
-    v.maxLength(5, ERR_ID_EQUIPO_AUX_MAX),
-)
+  v.array(v.number(ARRAY_ERROR)),
+  v.minLength(1, ERR_ID_EQUIPO_AUX),
+  v.maxLength(5, ERR_ID_EQUIPO_AUX_MAX)
+);
 
-const columna = v.pipe(
-    v.number(ERR_ID_COLUMNAS),
-    v.integer(ERR_ID_COLUMNAS),
-)
+const columna = v.pipe(v.number(ERR_ID_COLUMNAS), v.integer(ERR_ID_COLUMNAS));
 
 export const servicioLuzSchema = v.object({
-    fechaInstalacion: fechaInstalacion,
-    luminarias: luminaria,   // Ahora coincide con req.body
-    equipoAuxiliar: equipoAux,
-    columna: v.optional(columna),
-    mantenimiento: v.optional(v.array(id)),   // Si envías mantenimientos
+  fechaInstalacion: fechaInstalacion,
+  luminarias: luminaria, // Ahora coincide con req.body
+  equipoAuxiliar: equipoAux,
+  columna: v.optional(columna),
+  mantenimiento: v.optional(v.array(id)), // Si envías mantenimientos
 });
 
-export const servicioLuzOpcional = v.partial(servicioLuzSchema)
+export const servicioLuzOpcional = v.partial(servicioLuzSchema);
 
-export const validarServicioLuz = v.safeParserAsync(servicioLuzSchema)
-export const validarServicioLuzOpcional = v.safeParserAsync(servicioLuzOpcional)
+export const validarServicioLuz = v.safeParserAsync(servicioLuzSchema);
+export const validarServicioLuzOpcional =
+  v.safeParserAsync(servicioLuzOpcional);
